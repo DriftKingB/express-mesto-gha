f8df0e1795a3e9c87058bc36578b51966af64f7b
Apple enjoyer <maksim@MacBook-Pro-Maksim.local>
2022-09-13 17:01:47 +0300

    PR 13 (fixed)

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -2,49 +2,49 @@ const errorMessageTranslations = require('./errorMessageTranslations');
 
 class ErrorHandler {
   constructor() {
-    this._errorMessage = '';
+    this.errorMessage = '';
   }
 
-  _sendResponse(res) {
-    res.status(this._statusCode).send({ message: this._errorMessage });
+  sendResponse(res) {
+    res.status(this.statusCode).send({ message: this.errorMessage });
 
-    this._errorMessage = '';
-    this._statusCode = null;
+    this.errorMessage = '';
+    this.statusCode = null;
   }
 
   handleValidationError(err, res) {
-    this._statusCode = 400;
+    this.statusCode = 400;
 
     Object.values(err.errors).forEach((error) => {
       if (errorMessageTranslations[error.kind]) {
-        this._errorMessage = `${this._errorMessage}${error.path}: ${errorMessageTranslations[error.kind]}; `;
+        this.errorMessage = `${this.errorMessage}${error.path}: ${errorMessageTranslations[error.kind]}; `;
         return;
       }
-      this._errorMessage[error.path] = 'Неизвестная ошибка валидации';
+      this.errorMessage[error.path] = 'Неизвестная ошибка валидации';
     });
 
-    this._sendResponse(res);
+    this.sendResponse(res);
   }
 
   handleCastError(res) {
-    this._statusCode = 400;
-    this._errorMessage = 'Указан некорректный id';
+    this.statusCode = 400;
+    this.errorMessage = 'Указан некорректный id';
 
-    this._sendResponse(res);
+    this.sendResponse(res);
   }
 
   handleNotFoundError(res, errorMessage) {
-    this._statusCode = 404;
-    this._errorMessage = errorMessage;
+    this.statusCode = 404;
+    this.errorMessage = errorMessage;
 
-    this._sendResponse(res);
+    this.sendResponse(res);
   }
 
   handleUnknownError(res) {
-    this._statusCode = 500;
-    this._errorMessage = 'Неизвестная ошибка';
+    this.statusCode = 500;
+    this.errorMessage = 'Неизвестная ошибка';
 
-    this._sendResponse(res);
+    this.sendResponse(res);
   }
 }
 
